7e9e196b5b8dc0cac21f357fc528220dabaddd4a
Lillith Fox <imlvnaa@gmail.com>
2025-01-31 18:17:56 -0500

    build: both of these are promises :p

--- build.mjs
+++ build.mjs
@@ -1,23 +1,24 @@
 import * as esbuild from "esbuild";
 import { exec } from "pkg";
+(async () => {
+  console.log("Building...");
 
-console.log("Building...");
-
-esbuild.build({
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  outfile: "dist/index.js",
-  platform: "node",
-  target: ["node21"],
-  sourcemap: true,
-  loader: {
-    ".node": "copy",
-  },
-});
+  await esbuild.build({
+    entryPoints: ["src/index.ts"],
+    bundle: true,
+    outfile: "dist/index.js",
+    platform: "node",
+    target: ["node21"],
+    sourcemap: true,
+    loader: {
+      ".node": "copy",
+    },
+  });
 
 
-if (process.argv.includes("--exe")) {
-  console.log("Packaging...");
-  exec(["dist/index.js", "--target", "latest", "--output", "dist/OsuZapZap.exe"])
-}
-console.log("Done!");
+  if (process.argv.includes("--exe")) {
+    console.log("Packaging...");
+    await exec(["dist/index.js", "--target", "latest", "--output", "dist/OsuZapZap.exe"])
+  }
+  console.log("Done!");
+})();
